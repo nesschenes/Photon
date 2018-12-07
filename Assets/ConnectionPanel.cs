c382d786bfe2527c9ede682d7c8fa7b92c659567
NessChen <jaes54321@gmail.com>
2018-12-07 18:06:35 +0800

    Basic Photon server
    1.Import Photon server pipeline.
    2.Create sample scene to test conntection.

---
+++ Assets/ConnectionPanel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectionPanel : MonoBehaviour
+{
+    [SerializeField]
+    InputField m_PlayerNameInf = null;
+    [SerializeField]
+    Button m_ConnectBtn = null;
+
+    string mPlayerName = "";
+
+    void Awake()
+    {
+        m_PlayerNameInf.onValueChanged.AddListener(OnPlayerNameChanged);
+        m_ConnectBtn.onClick.AddListener(OnConnect);
+    }
+
+    void OnDestroy()
+    {
+        m_PlayerNameInf.onValueChanged.RemoveListener(OnPlayerNameChanged);
+        m_ConnectBtn.onClick.RemoveListener(OnConnect);
+    }
+
+    void OnPlayerNameChanged(string _content)
+    {
+        mPlayerName = _content;
+    }
+
+    void OnConnect()
+    {
+        Launcher.Instance.Connect(mPlayerName);
+    }
+}
---
+++ Assets/Launcher.cs
@@ -0,0 +1,61 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public static Launcher Instance { get; private set; }
+
+    void Awake()
+    {
+        Instance = this;
+
+        // master client will sync all clients to load level.
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public void Connect(string _playerName)
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            Debug.Log("Already Connected!");
+            return;
+        }
+
+        PhotonNetwork.NickName = _playerName;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    #region PUN Callbacks
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connect.. Successful!");
+
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogFormat("Disconnect.. Reason : {0}", cause);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Join Room.. Successful!");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.LogFormat("Join Room.. Faild! OperationCode : {0}, Reason : {0}", message);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.LogFormat("Join Random Room.. Faild! OperationCode : {0}, Reason : {0}", message);
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions());
+    }
+
+    #endregion
+}
