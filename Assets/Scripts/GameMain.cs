426838fc93f22fd009a8eb19cab906c0c4c288c9
CHEN-WEI-SYUAN <jaes_54321@yahoo.com.tw>
2018-12-09 15:19:00 +0800

    Add Game Scene
    1.Add simple gama scene.
    2.Relocate scripts to Scripts folder.
    3.Rename SamepleScene to Login.

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMain : MonoBehaviourPunCallbacks
+{
+    IEnumerator LoadScene_co()
+    {
+        var op = SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+
+        while(!op.isDone)
+            yield return null;
+    }
+
+    #region PUN Callbacks
+
+    public override void OnLeftRoom()
+    {
+        StartCoroutine(LoadScene_co());
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/GamePanel.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePanel : MonoBehaviour
+{
+    [SerializeField]
+    Button m_LeaveBtn = null;
+
+    void Awake()
+    {
+        m_LeaveBtn.onClick.AddListener(OnLeave);
+    }
+
+    void OnDestroy()
+    {
+        m_LeaveBtn.onClick.RemoveListener(OnLeave);
+    }
+
+    void OnLeave()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+}
